c3f32645213523ca22e1cc64b8b0ee834e5e1ff2
Salman391 <57536227+Salman391@users.noreply.github.com>
2019-11-21 19:40:12 +0500

    Add files via upload

---
+++ 2.27.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<static_cast<int>('a')<<"\n";
+	cout<<static_cast<int>('/')<<"\n";
+	cout<<static_cast<int>('+')<<"\n";
+	cout<<static_cast<int>('K')<<"\n";
+	cout<<static_cast<int>('$');
+}
